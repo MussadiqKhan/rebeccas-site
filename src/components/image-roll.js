926b71a207753b02ec4dd9eafe85f15ef0a2f462
Musadiq Khan <musadiq.khan@gmail.com>
2020-07-22 03:20:58 +0500

    Update image-roll.js

--- src/components/image-roll.js
+++ src/components/image-roll.js
@@ -1,9 +1,5 @@
 import React from "react";
 import image1 from "../images/image1.png";
-import image2 from "../images/image2.png";
-import image3 from "../images/image3.png";
-import image4 from "../images/image4.png";
-import image5 from "../images/image5.png";
 
 const ImageGallery = () => {
   return (
