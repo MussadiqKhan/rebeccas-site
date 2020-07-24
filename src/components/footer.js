f96f724b04876b8f81294ae729a05a6e5dea6d09
MussadiqKhan <musadiq.khan@gmail.com>
2020-07-25 01:25:49 +0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,36 +6,35 @@ import linkedin from "../images/linkedin.png";
 const Footer = (props) => {
   return (
     <div>
-      <footer>
-        <div className="container" style={{ marginTop: "80px" }}>
-          <div>
-            <h4>
-              NEXT PROJECT{" "}
-              <span>
-                <img src={icon} alt="icon" />
-              </span>
-            </h4>
-            <a href={props.link}>
-              {" "}
+      <a href={props.link}>
+        <footer>
+          <div className="container" style={{ marginTop: "80px" }}>
+            <div>
+              <h4>
+                NEXT PROJECT{" "}
+                <span>
+                  <img src={icon} alt="icon" />
+                </span>
+              </h4>{" "}
               <h2>{props.title}</h2>
-            </a>
+            </div>
+            <div className="social">
+              <a href="mailto:rebeccayeap@berkeley.edu">
+                {" "}
+                <img
+                  src={email}
+                  alt="email"
+                  style={{ marginRight: "25px" }}
+                />{" "}
+              </a>
+              <a href="https://www.linkedin.com/in/rebeccayeapberkeley/">
+                {" "}
+                <img src={linkedin} alt="linkedin" />
+              </a>
+            </div>
           </div>
-          <div className="social">
-            <a href="mailto:rebeccayeap@berkeley.edu">
-              {" "}
-              <img
-                src={email}
-                alt="email"
-                style={{ marginRight: "25px" }}
-              />{" "}
-            </a>
-            <a href="https://www.linkedin.com/in/rebeccayeapberkeley/">
-              {" "}
-              <img src={linkedin} alt="linkedin" />
-            </a>
-          </div>
-        </div>
-      </footer>
+        </footer>
+      </a>
     </div>
   );
 };
